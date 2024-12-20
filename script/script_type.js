817e6482da5719fb6aaa8cbb2ae0207dcb3d8e51
Anton Kasaryn <kasarin.anton@gmail.com>
2024-12-20 14:29:03 +0100

    Update html + css
    Add cards for previous jobs
    Some fixes
    Move scripts for separated fiels

---
+++ script/script_type.js
@@ -0,0 +1,6 @@
+var typed =- new Typed(".auto-input", {
+    strings: ["Welcome to my portfolio", "I'm iOS Software Engineer", "Creating, inspiring, sharing. This is my portfolio"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+});
